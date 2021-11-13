9a8e5e4c9614977bf6b3ae180da52ef0d6706c34
Shermeen Ajani <shermeen.ajani@gmail.com>
2021-11-13 14:23:57 -0500

    final

--- EmployeeSQL/Employees._Schema.sql
+++ EmployeeSQL/Employees._Schema.sql
@@ -68,18 +68,19 @@ CREATE TABLE titles (
 
 select * from titles
 
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
 ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY(dept_no)
 REFERENCES departments (dept_no);
 
---ALTER TABLE departments ADD CONSTRAINT fk_departments_dept_no FOREIGN KEY(dept_no)
---REFERENCES dept_manager (dept_no);
-
 ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_emp_no FOREIGN KEY(emp_no)
-REFERENCES employees (emp_no);
-
-ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_no FOREIGN KEY(emp_no)
-REFERENCES salaries (emp_no);
+REFERENCES "employees" ("emp_no");
 
 ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_title_id FOREIGN KEY(emp_title_id)
-REFERENCES titles (title_id);
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
 
--- EmployeeSQL/Employees_Queries.sql
+++ EmployeeSQL/Employees_Queries.sql
@@ -4,12 +4,14 @@ select employees.emp_no, employees.last_name, employees.first_name, employees.se
 from employees
 left join salaries
 on employees.emp_no = salaries.emp_no
+order by emp_no
 
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
 --https://www.postgresqltutorial.com/postgresql-date_part/
 select first_name, last_name, hire_date
 from employees
 where date_part ('year',hire_date) ='1986'
+order by last_name
 
 -- 3. List the manager of each department with the following information: department number, department name, 
 --the manager's employee number, last name, first name.
@@ -19,22 +21,26 @@ left join dept_manager
 on departments.dept_no = dept_manager.dept_no
 left join employees
 on dept_manager.emp_no = employees.emp_no
+order by dept_name, last_name
 
 -- 4. List the department of each employee with the following information: employee number, 
 --last name, first name, and department name.
-select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+select departments.dept_name, employees.emp_no, employees.last_name, employees.first_name 
 from employees
 left join dept_emp
 on employees.emp_no = dept_emp.emp_no
 left join departments
 on dept_emp.dept_no = departments.dept_no
+order by dept_name, last_name
 
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" and 
 --last names begin with "B."
+--https://www.postgresqltutorial.com/postgresql-like/
 select first_name, last_name, sex
 from employees
 where first_name = 'Hercules' and
 last_name like 'B%'
+order by last_name
 
 -- 6. List all employees in the Sales department, including their employee number, 
 --last name, first name, and department name.
@@ -45,6 +51,7 @@ on employees.emp_no = dept_emp.emp_no
 left join departments
 on dept_emp.dept_no = departments.dept_no
 where departments.dept_name = 'Sales'
+order by last_name
 
 -- 7. List all employees in the Sales and Development departments, 
 --including their employee number, last name, first name, and department name.
@@ -55,6 +62,7 @@ on employees.emp_no = dept_emp.emp_no
 left join departments
 on dept_emp.dept_no = departments.dept_no
 where departments.dept_name  = 'Sales' or departments.dept_name = 'Development'
+order by dept_name, last_name
 
 -- 8. In descending order, list the frequency count of employee last names, 
 --i.e., how many employees share each last name.
